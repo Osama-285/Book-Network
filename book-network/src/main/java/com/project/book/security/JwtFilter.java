fcb54c204e438ef391b928cc7692cca0ac5d24fe
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-05-12 13:40:37 +0500

    token

---
+++ book-network/src/main/java/com/project/book/security/JwtFilter.java
@@ -0,0 +1,8 @@
+package com.project.book.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtFilter {
+
+}
---
+++ book-network/src/main/java/com/project/book/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.project.book.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.project.book.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+}
