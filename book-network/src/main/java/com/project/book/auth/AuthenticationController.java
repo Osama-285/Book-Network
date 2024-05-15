c08ac79c6458c0a51564b4289f6cd40b5204d730
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-05-15 09:57:57 +0500

    Adding Auth manager

--- book-network/src/main/java/com/project/book/auth/AuthenticationController.java
+++ book-network/src/main/java/com/project/book/auth/AuthenticationController.java
@@ -31,11 +31,11 @@ public class AuthenticationController {
         return ResponseEntity.accepted().build();
     }
 
-    // @PostMapping("/authenticate")
-    // public ResponseEntity<AuthenticationResponse> authenticate(
-    // @RequestBody AuthenticationRequest request) {
-    // return ResponseEntity.ok(service.authenticate(request));
-    // }
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
 
     // @GetMapping("/activate-account")
     // public void confirm(
