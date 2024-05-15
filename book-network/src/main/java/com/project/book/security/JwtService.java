7b1a4a55a839ef078ff272129194bd5433fcefbb
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-05-15 09:19:31 +0500

    fixing minor bugs

--- book-network/src/main/java/com/project/book/security/JwtService.java
+++ book-network/src/main/java/com/project/book/security/JwtService.java
@@ -18,6 +18,7 @@ import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
+
     @Value("${application.security.jwt.secret-key}")
     private String secretKey;
     @Value("${application.security.jwt.expiration}")
