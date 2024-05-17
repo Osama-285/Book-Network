96d3d4cb77d552ce422cec28ba5c238090e6efb1
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-05-17 07:45:09 +0500

    updating feedback class

--- book-network/src/main/java/com/project/book/feedback/Feedback.java
+++ book-network/src/main/java/com/project/book/feedback/Feedback.java
@@ -1,18 +1,12 @@
 package com.project.book.feedback;
 
-import java.time.LocalDateTime;
-
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-
+import com.project.book.book.Book;
 import com.project.book.common.BaseEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,8 +21,11 @@ import lombok.experimental.SuperBuilder;
 @Entity
 public class Feedback extends BaseEntity {
 
+    @Column
     private Double note;
-
     private String comment;
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
 
 }
