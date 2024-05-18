2413b97dac32b85d71d94d4c8622867578a1a2e6
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-05-18 18:48:42 +0500

    book controller

--- book-network/src/main/java/com/project/book/book/BookController.java
+++ book-network/src/main/java/com/project/book/book/BookController.java
@@ -1,9 +1,15 @@
 package com.project.book.book;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.project.book.common.PageResponse;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.security.core.Authentication;
 
@@ -23,5 +29,19 @@ public class BookController {
             @Valid @RequestBody BookRequest request,
             Authentication connectedUser) {
         return ResponseEntity.ok(service.save(request, connectedUser));
+    }
+
+    @GetMapping("/{book-id}")
+    public ResponseEntity<BookResponse> findBookById(
+            @PathVariable("book-id") Integer bookId) {
+        return ResponseEntity.ok(service.findById(bookId));
+    }
+
+    @GetMapping
+    public ResponseEntity<PageResponse<BookResponse>> findAllBooks(
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication connectedUser) {
+        return ResponseEntity.ok(service.findAllBooks(page, size, connectedUser));
     }
 }
