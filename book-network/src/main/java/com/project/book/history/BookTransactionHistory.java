35f76b4a1d45ceb05366e83cd6e5731563b98b7d
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-05-20 23:12:35 +0500

    book history

--- book-network/src/main/java/com/project/book/history/BookTransactionHistory.java
+++ book-network/src/main/java/com/project/book/history/BookTransactionHistory.java
@@ -2,8 +2,11 @@ package com.project.book.history;
 
 import com.project.book.book.Book;
 import com.project.book.common.BaseEntity;
+import com.project.book.user.User;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,7 +21,12 @@ import lombok.experimental.SuperBuilder;
 @Entity
 public class BookTransactionHistory extends BaseEntity {
 
-    // private Book book;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
     private boolean returned;
     private boolean returnApproved;
 }
