6e6ea7d4d6dc9120a170ab939034125e2be0f19c
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-05-31 19:51:55 +0500

    app-module

--- book-network-ui/src/app/app.module.ts
+++ book-network-ui/src/app/app.module.ts
@@ -2,11 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterOutlet],
-  providers: [],
+  imports: [
+    BrowserModule,
+    RouterOutlet,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+  ],
+  providers: [HttpClient],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
