7eb494a3a1123690dbe3e1f0e8e4d1b47bfc7b4e
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-07-02 20:23:52 +0500

    app routing

--- book-network-ui/src/app/app-routing.module.ts
+++ book-network-ui/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
-// import { routes } from './app.routes';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
@@ -15,6 +15,7 @@ const routes: Routes = [
     component: RegisterComponent,
   },
 ];
+
 @NgModule({
   declarations: [],
   imports: [RouterModule.forRoot(routes)],
